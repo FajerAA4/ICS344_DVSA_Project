5afd31e30666892176326577e64bd50230b5aff9
Fajer Alyami <s202280160@kfupm.edu.sa>
2026-05-03 01:33:48 +0300

    Update order-manager-vulnerable.js

--- lesson-9-dependencies/code/order-manager-vulnerable.js
+++ lesson-9-dependencies/code/order-manager-vulnerable.js
@@ -6,15 +6,15 @@
 // unserialize(), which evaluates any function embedded with the
 // _$$ND_FUNC$$_ marker.
 
-const serialize = require('node-serialize');                                     // <-- vulnerable dep
+const serialize = require('node-serialize');                                     //vulnerable dep
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
 const { CognitoIdentityProviderClient, AdminGetUserCommand } = require("@aws-sdk/client-cognito-identity-provider");
 const jose = require('node-jose');
 
 
 exports.handler = (event, context, callback) => {
-    var req     = serialize.unserialize(event.body);     // <-- RCE entrypoint
-    var headers = serialize.unserialize(event.headers);  // <-- RCE entrypoint
+    var req     = serialize.unserialize(event.body);     
+    var headers = serialize.unserialize(event.headers);  
     var auth_header = headers.Authorization || headers.authorization;
     var token_sections = auth_header.split('.');
     var auth_data = jose.util.base64url.decode(token_sections[1]);
